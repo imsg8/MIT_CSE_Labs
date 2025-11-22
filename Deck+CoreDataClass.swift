6035e766591e23622356fed83c1d01852781fdf5
Shivang Gulati <shivanggulati817@gmail.com>
2025-11-23 01:32:41 +0530

    hello

---
+++ Deck+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  Deck+CoreDataClass.swift
+//  flashcards
+//
+//  Created by Shivang Gulati on 12/11/25.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias DeckCoreDataClassSet = NSSet
+
+
+public class Deck: NSManagedObject {
+
+}
